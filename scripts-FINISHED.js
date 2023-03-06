05a744b266ce6afd694309f3d7cdf41679e5787c
blameleo <leonardgre@gmail.com>
2023-03-06 12:38:51 +0000

    fixed functionality

--- scripts-FINISHED.js
+++ scripts-FINISHED.js
@@ -1,77 +1,59 @@
 const displayTimer = document.querySelector(".display__time-left");
 const displayEndTimer = document.querySelector(".display__end-time");
-const input = document.getElementById('custom')
-
+const input = document.getElementById("custom");
 
 const buttons = document.querySelectorAll("[data-time]");
-let countdown;
+let startTimer;
 
 for (let button of buttons) {
   button.addEventListener("click", (e) => {
     clearInterval(startTimer);
     let seconds = e.target.dataset.time;
-    timer(seconds);
-    
+    countDownTime(seconds);
   });
 }
 
-input.addEventListener('keypress',(e)=>{
- 
-if( e.key === "Enter"){
-  e.preventDefault()
-  let val = input[0].value * 60
-  clearInterval(countdown);
-  timer(val);
-  input[0].value = ""
-}
+input.addEventListener("keypress", (e) => {
+  if (e.key === "Enter") {
+    e.preventDefault();
+    let val = input[0].value * 60;
+    clearInterval(startTimer);
+    countDownTime(val);
+    input[0].value = "";
+  }
+});
 
-})
+function countDownTime(time) {
+  // let minute = seconds < 60 ? 0 : seconds / 60;
+  // seconds = seconds < 60 ? seconds : 0;
 
-
-function timer(seconds) {
-  let minute = seconds < 60 ? 0 : seconds / 60;
-  seconds = seconds < 60 ? seconds : 0;
+  const current = Date.now();
+  const then = current + time * 1000;
+  console.log(then);
 
   startTimer = setInterval(() => {
-    if (minute>=0) {
-      if (seconds == 0) {
-        displayTimer.textContent = `${minute}:${
-          seconds < 10 ? "0" : ""
-        }${seconds}`;
-        seconds = 59;
-        minute--;
-        timeLeft(seconds,minute)
-      } else {
-        displayTimer.textContent = `${minute}:${
-          seconds < 10 ? "0" : ""
-        }${seconds}` ;
-        seconds--;
-        timeLeft(seconds,minute)
-      }
-    } 
+    displayEndTime(then);
+
+    let minutes = Math.floor(time / 60);
+    let seconds = Math.floor(time % 60);
+
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+    displayTimer.textContent = `${minutes}: ${seconds}`;
+    time--;
+    if (minutes <= 0 && seconds <= 0) {
+      clearInterval(startTimer);
+      //  time = startingTime2 * 60;
+    }
   }, 1000);
-
-
-
 }
 
-
-function timeLeft (seconds,minute) {
-  const currentDate = new Date()
-  let hour = currentDate.getHours()
-  let minutes = currentDate.getMinutes() + minute
-  let second = currentDate.getSeconds() + seconds
-
-  if (minutes>60){
-    hour = hour + 1
-    minutes = minutes - 60
-    displayEndTimer.textContent = ` be back at ${hour} : ${minutes}`
-
-  } else {
-    displayEndTimer.textContent = ` be back at ${hour} : ${minutes}`
-
-  }
-
-
-  
+function displayEndTime(timestamp) {
+  const end = new Date(timestamp);
+  const hour = end.getHours();
+  const adjustedHour = hour > 12 ? hour - 12 : hour;
+  const minutes = end.getMinutes();
+  displayEndTimer.textContent = `Be Back At ${adjustedHour}:${
+    minutes < 10 ? "0" : ""
+  }${minutes}`;
 }
